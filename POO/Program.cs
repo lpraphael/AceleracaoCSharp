a337478a914c7bfbf2d1d32ddde045cdae02d0f4
lpraphael <raphael.perazzo@gmail.com>
2024-09-20 18:37:41 -0300

    feat: criando classes e instanciando-as

---
+++ POO/Program.cs
@@ -0,0 +1,19 @@
+﻿using Classes;
+
+public class Program
+{
+  public static void Main()
+  {
+    // Client raphael = new() // uma das formas de instanciar um objeto de classe Client
+    var raphael = new Client();
+    var leilanne = new Client();
+    raphael.Name = "Raphael";
+    leilanne.Name = "Leilanne";
+    Console.WriteLine("Objeto de Classe Client instanciado implicitamente e atribuido valor: " + raphael.Name);
+    Console.WriteLine("Objeto de Classe Client instanciado implicitamente e atribuido valor: " + leilanne.Name);
+    User userRaphael = new User("Raphael Lyra Perazzo");
+    User userLeilanne = new User("Leilanne Cristina");
+    Console.WriteLine("Objeto de Classe Client instanciado explicitamente e atribuido valor: " + userRaphael.Name);
+    Console.WriteLine("Objeto de Classe Client instanciado explicitamente e atribuido valor: " + userLeilanne.Name);
+  } // Exemplo de inicialização de classe com Constructor Explicito
+}
